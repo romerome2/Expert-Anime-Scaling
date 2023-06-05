8e41a1f725ba916fe17e4bf1c7fc10c4338cf907
Jerome Spooner <jspooner@brooklynsteamcenter.org>
2023-06-05 14:52:34 -0400

    added a new api

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,37 @@ import {ref, watch} from 'vue'
 </script>
 
 <template>
-  <nav>
-   <router-link to="/Home">Home</router-link>/
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+        color="red"
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar=""
+            title="Anime Scaling"
+            subtitle=""
+          ></v-list-item>
+        </v-list>
 
-   <router-link to="/about">about</router-link>/
+        <v-divider></v-divider>
 
-   <router-link to="/sign-up">signup</router-link>/
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder"><router-link to="/">home </router-link></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" ><router-link to="/about">About</router-link></v-list-item>
+          <v-list-item prepend-icon="mdi-star"  ><router-link to="/sign-up">Scale</router-link></v-list-item>
+          <v-list-item prepend-icon="mdi-star"  ><router-link to="/debate">Debates</router-link></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-  </nav>
+      <v-main style="height: 0px"></v-main>
+    </v-layout>
+  </v-card>
+  
   <router-view />
+
 </template>
 
 <style scoped>
